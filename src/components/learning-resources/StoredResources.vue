5e966c4c5d04d2dc3d11262ba598e002dfcdcb90
henrilics <henrydeguzman.java73@gmail.com>
2020-11-09 08:20:52 +0800

    updates

---
+++ src/components/learning-resources/StoredResources.vue
@@ -0,0 +1,15 @@
+<template>
+     <ul>
+          <learning-resource :link="res.link" :title="res.title" :description="res.description" :key="res.id" v-for="res in resources"></learning-resource> 
+     </ul>
+</template>
+
+<script>
+import LearningResource from './LearningResource.vue';
+export default {
+     props: ['resources'],
+     components: {
+          LearningResource
+     }
+}
+</script>
